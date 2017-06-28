5787e481198b38da8b07abfa052b3af2f69d4939
Martin Müller <martin.mathias.mueller@gmail.com>
2017-06-28 15:46:01 +0200

    change ES initializer

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,16 +1,26 @@
-Crowdbreaks::Client = Elasticsearch::Client.new(
-  hosts: [
-    {
-      host: ENV["ELASTICSEARCH_HOST"],
-      port: ENV["ELASTICSEARCH_PORT"],
-      scheme: 'http'
-    }
-  ]
-)
+require 'faraday_middleware/aws_signers_v4'
+# Crowdbreaks::Client = Elasticsearch::Client.new(
+#   hosts: [
+#     {
+#       host: ENV["ELASTICSEARCH_HOST"],
+#       port: ENV["ELASTICSEARCH_PORT"],
+#       scheme: 'http'
+#     }
+#   ]
+# )
+#
+#
+# Searchkick.aws_credentials = {
+#   access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+#   secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+#   region: ENV['AWS_REGION'],
+#   port: 80,
+#   service_name: "es"
+# }
 
-
-Searchkick.aws_credentials = {
-  access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-  secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-  region: ENV['AWS_REGION']
-}
+Crowdbreaks::Client = Elasticsearch::Client.new url: ENV['ELASTICSEARCH_URL'] do |f|
+  f.request :aws_signers_v4,
+    credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY'], ENV['AWS_SECRET_ACCESS_KEY']),
+    service_name: 'es',
+    region: ENV['AWS_REGION']
+end
