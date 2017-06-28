9b39fb99234180fba5abfd8b05c075ea12b2555b
Martin Müller <martin.mathias.mueller@gmail.com>
2017-06-28 15:14:51 +0200

    setting region in elasticsearch config

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -12,5 +12,5 @@ Crowdbreaks::Client = Elasticsearch::Client.new(
 Searchkick.aws_credentials = {
   access_key_id: ENV["AWS_ACCESS_KEY_ID"],
   secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-  region: "us-east-1"
+  region: ENV['AWS_REGION']
 }
