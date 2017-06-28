9057257b0947cc8749aff6facb633fb71138873d
Martin Müller <martin.mathias.mueller@gmail.com>
2017-06-28 16:06:17 +0200

    bugfix

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,3 +1,4 @@
+require 'patron'
 require 'faraday_middleware/aws_signers_v4'
 require 'typhoeus/adapters/faraday'
 # Crowdbreaks::Client = Elasticsearch::Client.new(
@@ -18,10 +19,20 @@ require 'typhoeus/adapters/faraday'
 #   port: 80,
 #   service_name: "es"
 # }
+#
+# Crowdbreaks::Client = Elasticsearch::Client.new url: ENV['ELASTICSEARCH_URL'] do |f|
+#   f.request :aws_signers_v4,
+#     credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY'], ENV['AWS_SECRET_ACCESS_KEY']),
+#     service_name: 'es',
+#     region: ENV['AWS_REGION']
+# end
+#
+#
 
-Crowdbreaks::Client = Elasticsearch::Client.new url: ENV['ELASTICSEARCH_URL'] do |f|
+client = Elasticsearch::Client.new url: ENV['ELASTICSEARCH_URL'] do |f|
   f.request :aws_signers_v4,
     credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY'], ENV['AWS_SECRET_ACCESS_KEY']),
     service_name: 'es',
-    region: ENV['AWS_REGION']
+    region: 'us-east-1'
+  f.adapter :patron
 end
