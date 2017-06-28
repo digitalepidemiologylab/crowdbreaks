7a53a49f715d750e5520f021f4720313e84f8e3b
Martin Müller <martin.mathias.mueller@gmail.com>
2017-06-28 14:12:08 +0200

    add searchkick to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,7 @@
 #
 
 class User < ApplicationRecord
+  searchkick
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
