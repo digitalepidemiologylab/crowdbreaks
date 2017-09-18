39366e63ad2fe2f58cf96a84eb00450fdbca36a8
Martin Müller <martin.mathias.mueller@gmail.com>
2017-09-18 20:55:49 +0200

    add react

---
+++ app/javascript/components/container.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export class Container extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>This is my container component</p>
+      </div>
+    );
+  }
+}
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,10 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import React from 'react';
+import { render } from 'react-dom';
+import { Container } from './../components/Container';
+
+document.addEventListener('turbolinks:load', () => {
+  render(<Container/>, document.body.appendChild(document.createElement('div')));
+});
