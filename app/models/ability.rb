802f5703e4276335e50ee437014668c1f4a171d6
Martin Müller <martin.mathias.mueller@gmail.com>
2017-10-27 14:31:28 +0200

    add ability file

---
+++ app/models/ability.rb
@@ -0,0 +1,22 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+
+    user ||= User.new
+    if user.admin?
+      can :manage, :all
+    else
+      # default rule
+      can :read, :all
+
+      # test page
+      cannot :test, :page
+
+      # question sequence
+      can :show, :question_sequence
+      can :show, :mturk_question_sequence
+      can :create, Result
+    end
+  end
+end
