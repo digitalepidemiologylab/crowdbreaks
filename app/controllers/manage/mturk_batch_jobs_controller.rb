6c32c5e1ee34948884f9720863282ec7da207ff9
Martin Müller <martin.mathias.mueller@gmail.com>
2017-11-02 14:31:37 +0100

    task creation through file upload

--- app/controllers/manage/mturk_batch_jobs_controller.rb
+++ app/controllers/manage/mturk_batch_jobs_controller.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 module Manage
   class MturkBatchJobsController < BaseController
     def new
@@ -5,7 +7,7 @@ module Manage
     end
 
     def index
-      @mturk_batch_jobs = MturkBatchJob.all
+      @mturk_batch_jobs = MturkBatchJob.all.order('created_at DESC').page(params[:page]).per(10)
     end
 
     def show
@@ -15,21 +17,71 @@ module Manage
     def create
       @mturk_batch_job = MturkBatchJob.new(batch_params)
 
-      respond_to do |format|
-        if @mturk_batch_job.save
-          format.html { redirect_to(manage_mturk_batch_jobs_path, notice: 'Job successfully created')}
-        else
-          format.html { render :new }
-        end
+      # make sure file is valid
+      csv_file = @mturk_batch_job.job_file
+      if csv_file.content_type != 'text/csv'
+        @mturk_batch_job.errors.add(:job_file, 'File format has to be csv')
+        render :new and return
+      end
+      if @mturk_batch_job.number_of_assignments.to_i == 0 or @mturk_batch_job.number_of_assignments.to_i > 100
+        @mturk_batch_job.errors.add(:number_of_assignments, 'Number assignments canot be 0 or >100')
+        render :new and return
       end
 
+      if CSV.table(csv_file.path).count == 0
+        @mturk_batch_job.errors.add(:job_file, 'Unsuccessful. File was empty.')
+        render :new and return
+      end
+
+      if not file_content_valid?(csv_file.path)
+        @mturk_batch_job.errors.add(:job_file, 'One or more tweet IDs were invalid integers.')
+        render :new and return
+      end
+
+      if @mturk_batch_job.save
+        CSV.foreach(csv_file.path) do |line|
+          tweet_id = line[0].to_s
+          num_assignments = @mturk_batch_job.number_of_assignments.to_i
+          num_assignments.times do 
+            puts "create tas"
+            if @mturk_batch_job.id.present?
+              Task.create(tweet_id: tweet_id, mturk_batch_job_id: @mturk_batch_job.id, lifecycle_status: 'Not submitted')
+            end
+          end
+        end
+        respond_to do |format|
+          format.html { redirect_to(manage_mturk_batch_jobs_path, notice: 'Job successfully created')}
+        end
+      else
+        respond_to do |format|
+          format.html { 
+            render :new 
+          }
+        end
+      end
     end
 
+      def destroy
+        @mturk_batch_jobs = MturkBatchJob.find_by(id: params[:id])
+        @mturk_batch_jobs.destroy
+        redirect_to manage_mturk_batch_jobs_path
+      end
 
-    private
 
-    def batch_params
-      params.require(:mturk_batch_job).permit(:name, :sandbox)
+      private
+
+      def batch_params
+        params.require(:mturk_batch_job).permit(:name, :sandbox, :job_file, :number_of_assignments)
+      end
+
+      def file_content_valid?(path)
+        CSV.foreach(@mturk_batch_job.job_file.path) do |line|
+          tweet_id = line[0].to_s
+          if not tweet_id =~ /\A\d{1,}\z/
+            return false
+          end
+        end
+        return true
+      end
     end
   end
-end
--- app/models/mturk_batch_job.rb
+++ app/models/mturk_batch_job.rb
@@ -1,5 +1,12 @@
 class MturkBatchJob < ApplicationRecord
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
+  attr_accessor :job_file
+  attr_accessor :number_of_assignments
+
 
   validates :name, presence: true, uniqueness: {message: "Name must be unique"}
+  validates :job_file, presence: true
+
+  private 
+
 end
