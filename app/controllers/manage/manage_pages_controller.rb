e05eda839e0733f7289cec30e204a70759c6b108
Martin Müller <martin.mathias.mueller@gmail.com>
2017-12-04 22:41:39 +0100

    status service flask api

--- app/controllers/manage/manage_pages_controller.rb
+++ app/controllers/manage/manage_pages_controller.rb
@@ -6,6 +6,8 @@ module Manage
       @es_ready = Crowdbreaks::ESClient.ping
       api = FlaskApi.new
       @api_ready = api.ping
+      @api_es_ready = api.test('es')
+      @api_redis_ready = api.test('redis')
     end
   end
 end
