9f32480b10d05132c6d9640ff35faa539b438f84
Martin Müller <martin.mathias.mueller@gmail.com>
2018-01-15 08:53:04 +0100

    elasticsearch index controller

---
+++ app/controllers/manage/elasticsearch_indexes_controller.rb
@@ -0,0 +1,48 @@
+module Manage
+  class ElasticsearchIndexesController < BaseController
+    before_action :api_init
+
+    def new
+    end
+
+    def index
+      stats = @api.es_stats
+      @indexes = []
+      stats.each_pair do |k, v|
+        unless k.starts_with?('.')
+          @indexes << {'name': k, num_docs: v['total']['docs']['count'], size_bytes: v['total']['store']['size_in_bytes']} 
+        end
+      end
+    end
+
+    def create
+      p elasticsearch_indexes_params
+      response = @api.create_index(elasticsearch_indexes_params[:name])
+      respond_with_flash(response, elasticsearch_indexes_path)
+    end
+
+    private
+
+    def api_init
+      @api = FlaskApi.new
+    end
+
+    def elasticsearch_indexes_params
+      params.require(:elasticsearch_index).permit(:name)
+    end
+
+    def respond_with_flash(response, redirect_path)
+      if response.success?
+        respond_to do |format|
+          flash[:notice] = response.parsed_response
+          format.html { redirect_to redirect_path }
+        end
+      else
+        respond_to do |format|
+          flash[:alert] = response.parsed_response
+          format.html { redirect_to redirect_path }
+        end
+      end
+    end
+  end
+end
