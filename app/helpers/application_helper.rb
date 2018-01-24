54233a82dc5699c4fb796cc96a43b05ca0eaed2f
Martin Müller <martin.mathias.mueller@gmail.com>
2018-01-24 01:40:13 +0100

    redesign login/signup/account settings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,9 @@ module ApplicationHelper
     flash.each do |msg_type, message|
       msg_type = "success" if msg_type == "notice"
       msg_type = "danger" if msg_type == "alert"
-      concat(content_tag(:div, message, class: "alert alert-#{msg_type}") do
-        concat content_tag(:button, '&times;'.html_safe, class: "close", data: { dismiss: 'alert' })
+      concat(content_tag(:div, message, class: "alert alert-#{msg_type} alert-dismissible fade show", role: 'alert') do
         concat message
+        concat content_tag(:button, '&times;'.html_safe, class: "close", data: { dismiss: 'alert' }, aria: {label: 'Close'})
       end)
     end
     nil
