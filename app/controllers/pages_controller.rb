5ca32b40599376aaabf9ca3c6d559927b4660d86
Martin Müller <martin.mathias.mueller@gmail.com>
2018-01-26 17:40:11 +0100

    redesign projects explore page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @num_tweets_classified = Result.count
+    @num_tweets_classified = Result.distinct.count(:tweet_id)
     @translations = I18n.backend.send(:translations)[I18n.locale][:pages][:index][:leadline]
   end
 
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,6 +7,10 @@ class ProjectsController < ApplicationController
     @project = Project.friendly.find(params[:id])
     raise 'This project has nothing to show' unless @project.es_index_name == 'project_vaccine_sentiment' 
     @interval = 'hour'
+    @pro_vaccine_count = @project.results.joins(:answer).where(answers: {label: 'pro-vaccine'}).count
+    @anti_vaccine_count = @project.results.joins(:answer).where(answers: {label: 'anti-vaccine'}).count
+    @neutral_vaccine_count = @project.results.joins(:answer).where(answers: {label: 'neutral'}).count
+    @total_count = @pro_vaccine_count + @anti_vaccine_count + @neutral_vaccine_count
   end
 
   private
