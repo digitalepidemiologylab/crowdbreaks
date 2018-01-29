33a8d79278ffdab47ddf6219a9cb22639f408fec
Martin Müller <martin.mathias.mueller@gmail.com>
2018-01-29 01:20:51 +0100

    bugfix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
       query = current_user.results.joins(:project).where(projects: {es_index_name: 'project_vaccine_sentiment'})
       @pro_vaccine_count = query.joins(:answer).where(answers: {label: 'pro-vaccine'}).distinct.count(:tweet_id)
       @anti_vaccine_count = query.joins(:answer).where(answers: {label: 'anti-vaccine'}).distinct.count(:tweet_id)
-      @neutral_vaccine_count = query.joins(:answer).where(answers: {label: 'neutral-vaccine'}).distinct.count(:tweet_id)
+      @neutral_vaccine_count = query.joins(:answer).where(answers: {label: 'neutral'}).distinct.count(:tweet_id)
       @total_count_vaccine_sentiment = @pro_vaccine_count + @anti_vaccine_count + @neutral_vaccine_count
       @total_count = current_user.results.distinct.count(:tweet_id)
       @vaccine_sentiment_project = Project.find_by(es_index_name: 'project_vaccine_sentiment')
