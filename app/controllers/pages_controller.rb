9381b77e92c46192ff485e381fd1aaaf127e2f64
Martin Müller <martin.mathias.mueller@gmail.com>
2018-02-08 18:06:25 +0100

    fix counts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,11 +5,9 @@ class PagesController < ApplicationController
     if user_signed_in?
       # Project vaccine sentiment
       counts = current_user.results.joins(:project).where(projects: {es_index_name: 'project_vaccine_sentiment'}).joins(:answer).group('answers.label').count
-      if not counts.empty?
-        @pro_vaccine_count = counts['pro-vaccine'] || 0
-        @anti_vaccine_count = counts['anti-vaccine'] || 0
-        @neutral_vaccine_count = counts['neutral'] || 0
-      end
+      @pro_vaccine_count = counts['pro-vaccine'] || 0
+      @anti_vaccine_count = counts['anti-vaccine'] || 0
+      @neutral_vaccine_count = counts['neutral'] || 0
       @total_count_vaccine_sentiment = @pro_vaccine_count + @anti_vaccine_count + @neutral_vaccine_count
       @vaccine_sentiment_project = Project.find_by(es_index_name: 'project_vaccine_sentiment')
 
