e9e9091c7235e8f9d482f06bbe713e0ba62132c2
Martin Müller <martin.mathias.mueller@gmail.com>
2018-04-12 18:08:49 +0200

    preliminary fix to always display tweet

--- app/services/flask_api.rb
+++ app/services/flask_api.rb
@@ -102,18 +102,19 @@ class FlaskApi
 
   # tweets
   def get_tweet(project, user_id: nil)
-    data = {'user_id': user_id}
-    tweet_id = nil
-    handle_error do
-      resp = self.class.get('/tweet/new/'+project, query: data, timeout: 2)
-      tweet_id = resp.parsed_response
-    end
-    # If API is down, fetch a random tweet
-    if tweet_id.nil? or not tweet_id.scan(/\D/).empty?
-      puts 'API is down'
-      tweet_id = Result.limit(1000).order('RANDOM()').first.tweet_id.to_s
-    end
-    tweet_id
+    # data = {'user_id': user_id}
+    # tweet_id = nil
+    # handle_error do
+    #   resp = self.class.get('/tweet/new/'+project, query: data, timeout: 2)
+    #   tweet_id = resp.parsed_response
+    # end
+    # # If API is down, fetch a random tweet
+    # if tweet_id.nil? or not tweet_id.scan(/\D/).empty?
+    #   puts 'API is down'
+    #   tweet_id = Result.limit(1000).order('RANDOM()').first.tweet_id.to_s
+    # end
+    Result.limit(1000).order('RANDOM()').first.tweet_id.to_s
+    # tweet_id
   end
 
   def update_tweet(project, user_id, tweet_id)
