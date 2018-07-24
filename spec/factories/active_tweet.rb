ee6c786c6515d4a494845bf54fccca3d2ad127e9
Martin Müller <martin.mathias.mueller@gmail.com>
2018-07-24 17:09:15 +0200

    cleanup of spec files, old initializers

--- spec/factories/active_tweet.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :active_tweet do
-    tweet_id 847878099614171136
-  end
-end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -14,6 +14,5 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it {should belong_to(:answer_set)}
   it {should belong_to(:project)}
 end
