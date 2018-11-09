aaa463d10518faf5d55e44a891e4f8390b33fb97
Martin Müller <martin.mathias.mueller@gmail.com>
2018-11-09 13:15:09 +0100

    small fix in question text wrapping

--- app/javascript/components/qs/Question.js
+++ app/javascript/components/qs/Question.js
@@ -5,18 +5,33 @@ export const Question = (props) => {
   let instructionsSymbol;
   let buttonStyle = {border: 0, padding: 0, cursor: 'pointer'}
   if (props.hasInstructions) {
-    instructionsSymbol = <button
+    instructionsSymbol = 
+        <button
+          style={buttonStyle}
+          onClick={props.toggleQuestionInstructions}>
+          <i className='fa fa-question-circle' style={{color: '#212529'}}></i>
+        </button>
+  }
+  let instructionBtn = <button
       style={buttonStyle}
       onClick={props.toggleQuestionInstructions}>
-      <sup>&ensp;<i className='fa fa-question-circle' style={{color: '#212529'}}></i></sup>
+      <i className='fa fa-question-circle' style={{color: '#212529'}}></i>
     </button>
 
+  let question, question_first, question_last;
+  let question_split = props.question.split(' ');
+  if (question_split.length > 1) {
+    question_first = question_split.slice(0, question_split.length-1).join(' ')
+    question_last = question_split[question_split.length-1]
+    question = <div>{question_first}&nbsp;<span className='no-split'>{question_last}&nbsp;<sup>{instructionBtn}</sup></span></div>
+  } else {
+    question = <div className="no-split">{props.question}&nbsp;<sup>{instructionBtn}</sup></div>
   }
+
   return (
-    <div className='no-wrap'>
+    <div>
       <h2 className='mb-5'>
-        { props.question }
-        { instructionsSymbol }
+        {question}
       </h2>
     </div>
   );
