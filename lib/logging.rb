b7addc54779ff1f10a659aa0448e30c0856946d6
Martin Müller <martin.mathias.mueller@gmail.com>
2018-11-25 21:33:08 +0100

    rollbar error logging

--- lib/logging.rb
+++ lib/logging.rb
@@ -2,7 +2,13 @@ module Logging
   class LogFormatter < Logger::Formatter
     def call(severity, time, progname, msg)
       date_format = time.strftime("%Y-%m-%d %H:%M:%S")
-      "[#{date_format}] [#{severity}]: #{msg}\n"
+      msg = "[#{date_format}] [#{severity}]: #{msg}\n"
+      if severity == 'ERROR'
+        Rollbar.error(msg)
+      elsif severity == 'WARNING'
+        Rollbar.warning(msg)
+      end
+      msg
     end
   end
 end
