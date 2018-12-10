355a6f33faee205e0b8046641e21e43fbac88eb6
Martin Müller <martin.mathias.mueller@gmail.com>
2018-12-10 16:43:08 +0100

    change mturk notification messages

--- app/services/mturk_notification.rb
+++ app/services/mturk_notification.rb
@@ -17,7 +17,7 @@ class MturkNotification
       status_code: :unsuccessful,
       code: 'max_tasks_by_worker_reached',
       title_message: 'You have completed all work in this HIT group.',
-      message: 'This HIT (and future HITs in this group), cannot be completed since you have reached the maximum number of HITs which we allocate for a single worker. We kindly ask you to return the HIT and thank you for your understanding. You will be disqualified from this batch - however, you are very welcome to submit work in a later batch!',
+      message: 'This HIT (and future HITs in this group), cannot be completed since you have reached the maximum number of HITs which we allocate for a single worker. We kindly ask you to return the HIT and thank you for your work. You will be disqualified from this batch - however, you are very welcome to submit work in a later batch!',
     }
   end
 
@@ -26,7 +26,7 @@ class MturkNotification
       status_code: :unsuccessful,
       code: 'all_tasks_finished',
       title_message: 'All work in this batch has been completed.',
-      message: 'No work could be retrieved because you have completed all work in this batch. We kindly ask you to return the HIT. You will be disqualified from this batch only - you are very welcome to submit work in a later batch!',
+      message: 'No work could be retrieved because you have completed all work in this batch. We kindly ask you to return the HIT and thank you for your work. You are very welcome to submit work in a later batch!',
     }
   end
 
