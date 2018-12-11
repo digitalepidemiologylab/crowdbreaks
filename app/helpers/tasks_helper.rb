dcc977f8b7fc07082b0aa1a18f1bbb57da88e3b8
Martin Müller <martin.mathias.mueller@gmail.com>
2018-12-11 22:34:22 +0100

    view helper fixes

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,15 +1,14 @@
 module TasksHelper
   def results_exist_for_task?(task)
-    task = Task.find_by(id: task.id)
     task.results.count > 0
   end
 
   def mturk_cached_hit_exists?(task)
-    hit = MturkCachedHit.find_by(id: task.hit_id)
+    hit = MturkCachedHit.find_by(hit_id: task.hit_id)
     hit.present?
   end
 
   def get_mturk_cached_hit_id(task)
-    MturkCachedHit.find_by(id: task.hit_id).id
+    MturkCachedHit.find_by(hit_id: task.hit_id).id
   end
 end
