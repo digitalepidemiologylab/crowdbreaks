fbd4472b5f8ec330299ceca31b7e2b9269a23997
Martin Müller <martin.mathias.mueller@gmail.com>
2019-01-14 10:57:10 +0100

    pass user locale on sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -48,8 +48,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :locale])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :locale])
   end
 
   private
