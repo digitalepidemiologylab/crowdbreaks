0a1979873321a494ab912d546ff4efd0db873414
Martin Müller <martin.mathias.mueller@gmail.com>
2019-03-26 00:05:03 +0100

    don't use mandrill in test mode

--- config/initializers/mandrill.rb
+++ config/initializers/mandrill.rb
@@ -1,2 +1,4 @@
 require 'mandrill'
-Crowdbreaks::Mandrill = Mandrill::API.new ENV['MANDRILL_API_KEY']
+unless Rails.env.test?
+  Crowdbreaks::Mandrill = Mandrill::API.new ENV['MANDRILL_API_KEY']
+end
