0715a969dd0ee5524f5aa7c8aeb8c8a3807b24c1
Martin Müller <martin.mathias.mueller@gmail.com>
2019-05-14 15:21:15 +0200

    fix tests

--- spec/factories/project.rb
+++ spec/factories/project.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
   sequence(:title) { |n| "Title #{n}" }
+  sequence(:name) { |n| "project_#{n}" }
   factory :project do
     title 
+    name
     description { "Description" }
     public { false }
     trait :public do
