b7cd23e3772cde2696a0c9b178ce1c0344b61f42
Olesya Altunina <utanashati@mail.ru>
2022-02-20 21:26:14 +0100

    Closed #61 (#62)
    Updated config sync check

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -194,8 +194,8 @@ class ApisController < ApplicationController
   def upload_config
     authorize! :configure, :stream
     @projects = Project.primary.where(active_stream: true)
-    selected_params = %i[keywords lang locales es_index_name slug covid storage_mode image_storage_mode model_endpoints]
-    config = @projects.to_json(only: selected_params)
+    config_params = %i[keywords lang locales es_index_name slug covid storage_mode image_storage_mode model_endpoints]
+    config = @projects.to_json(only: config_params)
     UploadConfigJob.perform_later(config)
     respond_with_flash(
       Helpers::ApiResponse.new(
