cd1e76b07b8da7ac5c3e211817e599038ce5e089
Olesya Altunina <utanashati@mail.ru>
2022-07-15 13:15:04 +0200

    ES queries update #79

--- app/services/api/elasticsearch_api.rb
+++ app/services/api/elasticsearch_api.rb
@@ -21,10 +21,9 @@ module ElasticsearchApi
     api_key: { id: ENV['ES_API_KEY_ID'], api_key: ENV['ES_API_KEY'] }
   )
 
-  # @@es_client = Aws::ElasticsearchService::Client.new
-
   Stretchy.client = @@es_client
 
+  # Basic ES commands
   def ping_es
     handle_es_errors(occured_when: 'pinging ES') do
       Timeout.timeout(TIMEOUT) do
@@ -49,12 +48,11 @@ module ElasticsearchApi
     end
   end
 
+  # ES queries
   def get_all_data(
     index:, keywords: nil, not_keywords: nil,
     start_date: 'now-20y', end_date: 'now', interval: 1.month, round_to_sec: nil, use_cache: true
   )
-    # start_date = parse_dates(start_date)
-    # end_date = parse_dates(end_date)
     start_date = process_date(start_date, round_to_sec)
     end_date = process_date(end_date, round_to_sec)
     keywords = keywords.nil? ? [] : keywords
@@ -64,13 +62,6 @@ module ElasticsearchApi
     definition = {
       aggs: { all_data_agg: { date_range: { field: 'created_at', format: 'strict_date_optional_time', ranges: ranges } } }
     }
-
-    # definition = {
-    #   aggs: {
-    #     all_data_agg: { date_histogram: { field: 'created_at', fixed_interval: interval, format: 'yyyy-MM-dd HH:mm:ss' } }
-    #   },
-    #   query: { bool: { filter: [{ range: { created_at: { gte: start_date, lte: end_date } } }] } }
-    # }
 
     definition[:query] = {} unless keywords.empty? && not_keywords.empty?
     keywords.each do |keyword|
@@ -193,47 +184,37 @@ module ElasticsearchApi
     end
   end
 
-  def tweets(index:, question_tag: 'sentiment', user_id: nil, new_prob: 0.0)
+  def tweets(index:, user_id: 0, new_prob: 0.5, start_days_back: 7, end_days_back: 0)
     query_new = { bool: { must_not: [{ exists: { field: 'annotations' } }] } }
     query_not_finished = { bool: {
       filter: [{ "script": {
         "script": "doc.containsKey('annotations.user_id') && doc['annotations.user_id'].size() > 0 && " \
                   "doc['annotations.user_id'].size() < #{MAX_ASSIGNMENTS}"
       } }],
-      # [
-      #   { exists: { field: 'annotations' } },
-      #   { script: { script: {
-      #     source: "doc['annotations'].value < params.max_assignments",
-      #     lang: 'painless',
-      #     params: {
-      #       max_assignments: MAX_ASSIGNMENTS
-      #     }
-      #   } } }
-      # ],
-      # must: [
-      #   { script: { script: "doc['annotations'].values.length < #{MAX_ASSIGNMENTS}" } }
-      # ],
       must_not: [{ terms: { 'annotations.user_id': [user_id] } }]
     } }
 
     query_pipeline = lambda do |query, index|
+      query = sample_predictions_definition(
+        recent_predictions_query(query, start_days_back: start_days_back, end_days_back: end_days_back)
+      )
       handle_es_errors(occured_when: 'fetching a new tweet from ES') do
         Timeout.timeout(TIMEOUT) do
-          @@es_client.search(
-            index: index, size: MAX_VALIDATIONS,
-            body: sample_predictions_definition(recent_predictions_query(query), question_tag)
-          )['hits']['hits']
+          @@es_client.search(index: index, size: MAX_VALIDATIONS, body: query)['hits']['hits']
         end
       end
     end
 
     rand_num = rand
+    # First try to get annotated tweets that are not finished
     tweets = rand_num > new_prob ? query_pipeline.call(query_not_finished, index) : query_pipeline.call(query_new, index)
-    return tweets if tweets.is_a? Helpers::ApiResponse
+    return tweets if tweets.is_a?(Helpers::ApiResponse) && tweets.error?
 
+    # If no tweets returned for query_not_finished, try query_new
     tweets = query_pipeline.call(query_new, index) if rand_num > new_prob && tweets.empty?
-    return tweets if tweets.is_a? Helpers::ApiResponse
+    return tweets if tweets.is_a?(Helpers::ApiResponse) && tweets.error?
 
+    # Get necessary attributes of the received tweets
     tweets = tweets.map { |tweet| Helpers::Tweet.new(id: tweet['_id'], text: tweet['_source']['text'], index: tweet['_index']) }.compact
     Helpers::ApiResponse.new(status: :success, body: tweets)
   end
@@ -329,11 +310,14 @@ module ElasticsearchApi
     round_to_seconds(date, round_to_sec)
   end
 
-  def recent_predictions_query(query, days_back: 7)
+  def recent_predictions_query(query, start_days_back: 7, end_days_back: 0)
+    gte = (Time.now.utc - start_days_back.days).strftime(DATE_FORMAT)
+    lte = (Time.now.utc - end_days_back.days).strftime(DATE_FORMAT)
     { bool: {
       must: [
-        { range: { created_at: { gte: (Time.now.utc - 60 * 60 * 24 * days_back).strftime(DATE_FORMAT) } } },
-        { exists: { field: 'predictions' } }
+        { range: { created_at: { gte: gte, lte: lte } } }
+        # Wanted to sample based on the prediction outputs, but too complicated for now. Commented for later, if needed
+        # { exists: { field: 'predictions' } }
       ],
       must_not: [
         { exists: { field: 'is_retweet' } },
@@ -348,15 +332,17 @@ module ElasticsearchApi
     time + remainder
   end
 
-  def sample_predictions_definition(query, question_tag)
-    {
-      query: { function_score: {
-        script_score: { script: {
-          source: "if (_score > params['_source']['predictions']['#{question_tag}']['endpoints']['primary_probability'])" \
-                  ' { _score } else { 0 }'
-        } },
-        query: { function_score: { boost_mode: 'replace', functions: [{ random_score: {} }], query: query } }
-      } }
-    }
+  def sample_predictions_definition(query)
+    { query: { function_score: { boost_mode: 'replace', functions: [{ random_score: {} }], query: query } } }
+    # Wanted to sample based on the prediction outputs, but too complicated for now. Commented for later, if needed
+    # {
+    #   query: { function_score: {
+    #     script_score: { script: {
+    #       source: "if (_score > params['_source']['predictions']['#{question_tag}']['endpoints']['primary_probability'])" \
+    #               ' { _score } else { 0 }'
+    #     } },
+    #     query: { function_score: { boost_mode: 'replace', functions: [{ random_score: {} }], query: query } }
+    #   } }
+    # }
   end
 end
