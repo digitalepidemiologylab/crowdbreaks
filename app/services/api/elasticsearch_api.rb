31e94916dc1c88a1b49780ca8b9c02251921d6d6
Olesya Altunina <utanashati@mail.ru>
2022-07-27 11:22:44 +0200

    Delete old stuff from ESApi

--- app/services/api/elasticsearch_api.rb
+++ app/services/api/elasticsearch_api.rb
@@ -273,15 +273,15 @@ module ElasticsearchApi
   #   Current user ID to exclude tweets that the user has already annotated.
   # [new_prob]
   #   Probability to get a new tweet, not an already annotated one.
-  # [start_days_back, end_days_back]
-  #   +start_days_back+ < query date range < +end_days_back+
+  # [start_date, end_date]
+  #   +start_date+ < query date range < +end_date+
   # [max_assignments]
   #   Maximum amount of users to annotate one tweet.
   # [max_validations]
   #   Output size of the search query, representing the capacity for tweet validations down the line
   #   (in Project#tweet).
   def tweets(
-    index:, user_id: 0, new_prob: 0.5, start_days_back: 7, end_days_back: 0, max_assignments: 2, max_validations: 5
+    index:, user_id: 0, new_prob: 0.5, start_date: 'now-1w', end_date: 'now', max_assignments: 2, max_validations: 5
   )
     query_new = { bool: { must_not: [{ exists: { field: 'annotations' } }] } }
     query_not_finished = { bool: {
@@ -294,7 +294,7 @@ module ElasticsearchApi
 
     query_pipeline = lambda do |query, index|
       query = sample_predictions_definition(
-        recent_predictions_query(query, start_days_back: start_days_back, end_days_back: end_days_back)
+        recent_predictions_query(query, start_date: start_date, end_date: end_date)
       )
       handle_es_errors(occured_when: 'fetching a new tweet from ES') do
         Timeout.timeout(TIMEOUT) do
@@ -363,20 +363,6 @@ module ElasticsearchApi
     }
   end
 
-  def get_ranges(start_date, end_date, interval)
-    dates = []
-    while end_date >= start_date
-      dates.append end_date.utc.strftime(DATE_FORMAT)
-      end_date -= Helpers::TimeParser.new(interval).time
-    end
-    dates = dates.reverse
-    ranges = []
-    dates.each_cons(2) do |start_date, end_date|
-      ranges << { from: start_date, to: end_date }
-    end
-    ranges
-  end
-
   def handle_es_errors(occured_when: nil)
     retries = 0
     begin
@@ -408,26 +394,11 @@ module ElasticsearchApi
     Time.parse(date).utc.strftime(DATE_FORMAT)
   end
 
-  def parse_date_datemath(date)
-    return Helpers::TimeParser.new(date).datetime if date.include? 'now'
-
-    Time.parse(date).utc
-  end
-
-  def process_date(date, round_to_sec)
-    date = parse_date_datemath(date)
-    return date if round_to_sec.nil?
-
-    round_to_seconds(date, round_to_sec)
-  end
-
-  def recent_predictions_query(query, start_days_back: 7, end_days_back: 0)
-    gte = (Time.now.utc - start_days_back.days).strftime(DATE_FORMAT)
-    lte = (Time.now.utc - end_days_back.days).strftime(DATE_FORMAT)
+  def recent_predictions_query(query, start_date: 'now-1w', end_date: 'now')
     { bool: {
       must: [
-        { range: { created_at: { gte: gte, lte: lte } } }
-        # Wanted to sample based on the prediction outputs, but too complicated for now. Commented for later, if needed
+        { range: { created_at: { gte: start_date, lte: end_date } } }
+        # Wanted to sample based on the prediction outputs (active learning), but too complicated for now. Commented for later, if needed
         # { exists: { field: 'predictions' } }
       ],
       must_not: [
@@ -437,15 +408,9 @@ module ElasticsearchApi
     } }.deep_merge(query)
   end
 
-  def round_to_seconds(time, seconds)
-    seconds = Integer(seconds)
-    remainder = seconds - (Integer(time) % seconds)
-    time + remainder
-  end
-
   def sample_predictions_definition(query)
     { query: { function_score: { boost_mode: 'replace', functions: [{ random_score: {} }], query: query } } }
-    # Wanted to sample based on the prediction outputs, but too complicated for now. Commented for later, if needed
+    # Wanted to sample based on the prediction outputs (active learning), but too complicated for now. Commented for later, if needed
     # {
     #   query: { function_score: {
     #     script_score: { script: {
