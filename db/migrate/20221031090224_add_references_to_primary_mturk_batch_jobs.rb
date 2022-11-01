2ce8bdb5dd04d768d5b23d5043cf2244575a81ac
Djilani Kebaili <djilani.kebaili@epfl.ch>
2022-11-01 14:51:43 +0100

    fix DB columns

--- db/migrate/20221031090224_add_references_to_primary_mturk_batch_jobs.rb
+++
@@ -1,8 +0,0 @@
-class AddReferencesToPrimaryMturkBatchJobs < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :primary_mturk_batch_jobs, :mturk_worker_qualification_list, foreign_key: true, index: {
-      name: 'index_primary_mturk_batch_jobs_on_mturk_qualification_list_id'
-    }
-    add_reference :primary_mturk_batch_jobs, :mturk_batch_job, foreign_key: true
-  end
-end
---
+++ db/migrate/20221031090224_move_mturk_references_from_batch_to_primary.rb
@@ -0,0 +1,16 @@
+class MoveMturkReferencesFromBatchToPrimary < ActiveRecord::Migration[5.2]
+  def change
+    add_reference(:primary_mturk_batch_jobs, :mturk_worker_qualification_list, foreign_key: true)
+
+    reversible do |dir|
+      dir.up do
+        add_reference(:primary_mturk_batch_jobs, :mturk_batch_job, foreign_key: true)
+        remove_column(:mturk_batch_jobs, :primary_mturk_batch_job_id)
+      end
+      dir.down do
+        add_reference(:mturk_batch_jobs, :primary_mturk_batch_job, foreign_key: true)
+        remove_column(:primary_mturk_batch_jobs, :mturk_batch_job_id)
+      end
+    end
+  end
+end
